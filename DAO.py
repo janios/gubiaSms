d3bc4dd99c98485d5b0b0d0715ee09e04017900f
janios <escamilla.cristian@gmail.com>
2020-12-16 22:00:46 -0600

    DAO, elimina y agrega whatsApp enviados

--- DAO.py
+++ DAO.py
@@ -4,7 +4,6 @@
 import pymssql
 import traceback
 
-logger = ""
 host = ""
 username = ""
 password = ""
@@ -22,26 +21,34 @@ class DAO():
         global password
         global database
         global tablaEntrada
+        global tablaWhatsAppCorrecto
+        global tablaNoEnviados
         global numeroRegistros
+        global conn
+      
         logger = loggerEntrada
         host = config['host']
         username = config['username']
         password = config['password']
         database = config['database']
         tablaEntrada = config['tablaEntrada']
+        tablaWhatsAppCorrecto = config['whatsAppSalida']
+        tablaNoEnviados = config['tablaNoEnviados']
         numeroRegistros = config['numeroRegistros']
-       
-    def obtenerMensajes(self):
-        global logger
-        global host
-        global username
-        global password
-        global database
-        global numeroRegistros
         try:
             conn = pymssql.connect(host, username, password, database)
+            logger.info("Base de Datos conectada")
+        except:
+            error = traceback.format_exc()
+            logger.logError("Error en la base de datos {}".format(error))
+        
+
+    def obtenerMensajes(self):
+        global logger
+        global conn
+
+        try:
             cursor = conn.cursor()  
-            logger.logInfo("Base conectada")
             registros= []
             consulta = "Select TOP("+str(numeroRegistros)+") * from " + tablaEntrada + " order by id"  
             logger.logInfo(consulta)
@@ -51,12 +58,55 @@ class DAO():
                 for col in row:
                     lista.append(col)
                 registros.append(lista)
-            conn.close()
             logger.logInfo("Registros obtenidos ->" + str(len(lista)))
             return registros
         except:
             error = traceback.format_exc()
-            logger.logError("Error en la base de datos")
-            logger.logError(error)
+            logger.logError("Error en la base de datos {}".format(error))
             return "Error"
+
+    def eliminar(self, tabla, id):
+        global conn
+        global logger
+
+        try:
+            cursor = conn.cursor()
+            query = "DELETE FROM {} WHERE id = '{}'".format(tabla, id)
+            logger.info("Ejecutando query -> {}".format(query))
+            cursor.execute(query)
+            conn.commit()
+            logger.info("Registro {} eliminado".format(id))
+            return "Registro {} eliminado".format(id)
+        except:
+            error = traceback.format_exc()
+            logger.logError("Error {} en la eliminacion de la tabla {} con el id {}".format(error, tabla, id))
+            return "ERROR"
+    
+    def desconectar(self):
+        global conn
+        global logger
+        logger.info("Coneccion cerrada")
+        conn.close()
+    
+    def eliminarMensajeEntrada(self, id):
+        global tablaEntrada
+        return self.eliminar(tablaEntrada, id)
+    
+    def agregarWhatsCorrecto(self, mensaje):
+        global tablaWhatsAppCorrecto
+        global logger
+        global conn
+
+        try:
+            cursor = conn.cursor()
+            query = "INSERT INTO {} (telefono, mensaje, fecha, Sucursal) values ('{}', '{}', '{}', {})".format(tablaWhatsAppCorrecto, mensaje[2], mensaje[3],mensaje[1],mensaje[5])
+            logger.info(query)
+            cursor.execute(query)
+            conn.commit()
+            logger.info("registro agregado a tabla {}".format(tablaWhatsAppCorrecto))
+            return "Registro Agregado"
+        except:
+            error = traceback.format_exc()
+            logger.logError("Error {} en la eliminacion de la tabla {} ".format(error, tablaWhatsAppCorrecto))
+            return "ERROR"   
 
--- PruebaDesarrollo.py
+++ PruebaDesarrollo.py
@@ -7,20 +7,19 @@ from logger import Logger
 from datetime import datetime
 from DAO import DAO
 from contactosV2 import Contactos
-#from whatsappV2 import Whatsapp
-from whatsapp import Whatsapp
+from whatsappV2 import Whatsapp
+
 
 config = yaml.safe_load(open('configuracion.yml'))
 
 logger = Logger(config['logger'])
-telegram = TelegramNotificaciones(config['gubiaBoot'], logger)
+#telegram = TelegramNotificaciones(config['gubiaBoot'], logger)
 #telegram.enviarMensaje("Hola Mundo Grupo desde YAML refactorizado actual " + str(datetime.now()) )
 #logger.logInfo("Prueba")
-#DAO = DAO(config['baseDatos'], logger)
-#mensajes = DAO.consulta()
 
-contactos = Contactos(config['peopleApi'], logger)
-resultado = contactos.agregarContacto("5554374058", "Cris")
+
+#contactos = Contactos(config['peopleApi'], logger)
+#resultado = contactos.agregarContacto("2223178965", "Andres")
 
 #resultados = contactos.listarContactos()
 #print resultados
@@ -33,8 +32,20 @@ resultado = contactos.agregarContacto("5554374058", "Cris")
 #print resultados
 
 #whatsApp = Whatsapp(config['whatsApp'], logger)
-whatsApp = Whatsapp()
-resultado = whatsApp.enviarWhats("5554374058" , "Prueba V2")
-print resultado
+#whatsApp = Whatsapp()
+#resultado = whatsApp.enviarWhats("2223178965" , "Este mensaje es automatico desde el sms V2 pero eso no te quita lo puto, ni que los pumas esten en la final, voy ver el futbol mañana sigo")
+#print resultado
 
+DAO = DAO(config['baseDatos'], logger)
+mensajes = DAO.obtenerMensajes()
+
+print len(mensajes)
+print mensajes[0][0]
+
+#resultado = DAO.eliminarMensajeEntrada(mensajes[0][0])
+
+#print resultado
+
+resultado = DAO.agregarWhatsCorrecto(mensajes[0])
+print resultado
 
